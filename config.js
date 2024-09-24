9124639f3a8821c3547f50de6499cbfd79aa7954
JazziB14 <bootht14@gmail.com>
2024-09-24 11:50:49 -0400

    adds the getPosts function to get post from supabase

--- config.js
+++ config.js
@@ -1,5 +1,24 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://xtxwmrmjrggeughetccr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0eHdtcm1qcmdnZXVnaGV0Y2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY3NjU2OTAsImV4cCI6MjA0MjM0MTY5MH0.4ZCb7HD6OsV2mp4YVPuxurRbt7w8JAkx5k2fMnLNH2g')
+const supabase =
+    createClient(
+        'https://xtxwmrmjrggeughetccr.supabase.co',
+        `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0eHdtcm1qcmdnZXVnaGV0Y2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY3NjU2OTAsImV4cCI6MjA0MjM0MTY5MH0.4ZCb7HD6OsV2mp4YVPuxurRbt7w8JAkx5k2fMnLNH2g`)
 export default supabase;
+
+// async function getAUrl(){
+//
+//         const response = await fetch('https://xtxwmrmjrggeughetccr.supabase.co');
+//         const data = await response.json();
+//
+//         console.log(data);
+//
+//         console.log(error);
+//     }
+// }) ();
+
+const { data, error} = await supabase
+    .from('f24_postsTable')
+    .select()
+    console.log(data)
--- posts.js
+++ posts.js
@@ -1,37 +1,15 @@
+import supabase from "./config.js";
 /**
  * Gets all the post in the database
  * @returns {[{publishedBy: string, publishedOn: string, id: number, title: string, content: string},{publishedBy: string, publishedOn: string, id: number, title: string, content: string},{publishedBy: string, publishedOn: string, id: number, title: string, content: string},{publishedBy: string, publishedOn: string, id: number, title: string, content: string}]}
  */
-export function getPosts() {
-    return [
-  {
-    "id": 1,
-    "title": "The Future of Web Development",
-    "publishedOn": "2024-07-15",
-    "publishedBy": "John Doe",
-    "content": "As we look ahead to the future of web development, exciting trends are emerging..."
-  },
-  {
-    "id": 2,
-    "title": "Mastering Responsive Design",
-    "publishedOn": "2024-07-10",
-    "publishedBy": "Jane Smith",
-    "content": "Responsive design is crucial in today's multi-device world. Here are some tips to master it..."
-  },
-  {
-    "id": 3,
-    "title": "The Power of CSS Grid",
-    "publishedOn": "2024-07-05",
-    "publishedBy": "Alex Johnson",
-    "content": "CSS Grid has revolutionized web layout. Let's explore its capabilities and best practices..."
-  },
-  {
-    "id": 4,
-    "title": "Looping in JS",
-    "publishedOn": "2024-08-05",
-    "publishedBy": "Alex Johnson",
-    "content": "This is an interesting example of looping..."
-  }]
+export async function getPosts() {
+
+let { data, error } = await supabase
+  .from('f24_postsTable')
+  .select('*')
+
+    return data;
 }
 
 /**
